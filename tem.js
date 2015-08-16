44d5d2d12ab894b89188a11f8acfa53ffbada864
陈彦成 <cycok@cycok.local>
2015-08-17 00:07:22 +0800

    last

--- tem.js
+++ tem.js
@@ -3,8 +3,17 @@ var fs = require('fs');
 var marked = require('marked');
 
 
-var str = marked(fs.readFileSync('data/a.mark','utf-8'));
 
+marked.setOptions({
+  highlight: function (code) {
+  	return require('highlight').highlight(code);
+    //return require('highlight').highlightAuto(code).value;
+  }
+});
+
+//var express = require('express');
+var str = marked(fs.readFileSync('data/a.mark','utf-8'));
+console.log(str);
 
 
 
