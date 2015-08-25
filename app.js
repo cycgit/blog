563cfb79f4fe3c14276dfd6f42f039dc2dd020e6
陈彦成 <cycok@cycok.local>
2015-08-25 22:00:54 +0800

    23

--- app.js
+++ app.js
@@ -1,43 +1,28 @@
 var express = require('express');
 var app = express();
 var hbs = require('hbs');
-<<<<<<< HEAD
-=======
 
 var marked = require('marked');
-var h = require('highlight');
 var db = require('./data/db');
 
 
 marked.setOptions({
     highlight: function (code) {
-        return h.Highlight(code);
+        return require('highlight').Highlight(code);
         //return require('highlight').highlightAuto(code).value;
     }
 });
-
-
->>>>>>> aacb3fc8139ab9224dca89d818d2117c6ffc9d37
 
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 app.engine('hbs', hbs.__express);
 
-<<<<<<< HEAD
-app.use('/static',express.static('static', {Mixed: false}));
-app.get('/favicon.ico', function (req, res) {
-        var data = require('fs').readFileSync('static/favicon.ico');
-        res.end(data);
-});
-=======
 hbs.registerPartials('views/blog');
 
 
 var fs = require('fs');
 
 app.use(['/static','/favicon.ico/'],express.static('static', {Mixed: false}));
->>>>>>> aacb3fc8139ab9224dca89d818d2117c6ffc9d37
-
 
 app.get(['/', '/index', '/home'], function (req, res) {
 
@@ -67,11 +52,6 @@ app.get('/blog/:key', function(req, res, next){
             });
 
     });
-
-<<<<<<< HEAD
-	res.render('blog');
-=======
->>>>>>> aacb3fc8139ab9224dca89d818d2117c6ffc9d37
 
 });
 
