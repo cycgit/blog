51afeba2c7d1547b2abe00c916540495ac72b858
cyc <342930982@qq.com>
2015-08-27 18:04:00 +0800

    23

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ app.get('/blog/:key', function(req, res, next){
 
     //返回文章
     db.Blog.findOne({_id:req.params.key}).populate('auth').exec(function(err, data){
-
         data.content = marked(data.content);
         res.render('blog', data);
     });
